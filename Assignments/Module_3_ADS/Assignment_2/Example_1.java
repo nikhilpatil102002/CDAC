4c567981d04cde2cde90983dfaec0ee9df0795a9
Nikhil Patil <130439368+nikhilpatil102002@users.noreply.github.com>
2024-09-29 21:53:30 +0530

    Add files via upload

---
+++ Assignments/Module_3_ADS/Assignment_2/Example_1.java
@@ -0,0 +1,34 @@
+package com.starpattern;
+
+public class Example_1 {
+
+	public static void main(String[] args) {
+		int n=3;
+		
+		
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("================================");
+		
+		int n1=5;
+		for(int i=1;i<=n1;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+		
+		System.out.println("================================");
+		  		
+	}
+}
+//		
+
+	
+
+
