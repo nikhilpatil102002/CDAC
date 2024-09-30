172d2629cfc89af700062c84817c68c8c6c30f5b
Nikhil Patil <130439368+nikhilpatil102002@users.noreply.github.com>
2024-09-30 09:08:25 +0530

    Add files via upload

---
+++ Assignments/Module_3_ADS/Assignment_2/Example_5.java
@@ -0,0 +1,12 @@
+package com.starpattern;
+
+public class Example_5 {
+
+	public static void main(String[] args) {
+		int arr[]= {4,5,6,7};
+		
+		for(int i=arr.length-1;i>=0;i--) {
+			System.out.print(arr[i]+" ");
+		}	
+	}
+}
