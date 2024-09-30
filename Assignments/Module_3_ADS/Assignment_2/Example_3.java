dd38354378ca350918f965f82441ca883af5f88a
Nikhil Patil <130439368+nikhilpatil102002@users.noreply.github.com>
2024-09-30 09:05:28 +0530

    Add files via upload

---
+++ Assignments/Module_3_ADS/Assignment_2/Example_3.java
@@ -0,0 +1,11 @@
+package com.starpattern;
+import java.util.*;
+public class Example_3 {
+	
+	public static void main(String[] args) {
+		String sr="Hello World";
+		String st=sr.replaceAll("\\s","");
+		System.out.println(st);
+		
+	}
+}
